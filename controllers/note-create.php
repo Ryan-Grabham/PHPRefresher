6fc14a340cc33d998705b266ee35ef9d1c75f643
Ryan-Grabham <gra18001287@btc.ac.uk>
2023-10-18 15:25:47 +0100

    note-validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,31 @@
-<?php 
+<?php
+
+require 'Validator.php';
+
+$config = require 'config.php';
+$db = new Database($config['database']);
 
 $heading = 'Create a new note';
 
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+
+    $errors = [];
+
+    if (! Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'The note must be between 1 and 1000 characters long.';
+    }
+
+
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 4
+        ]);
+    }
+
+
 }
 
 require 'views/note-create.view.php';
--- views/note.view.php
+++ views/note.view.php
@@ -8,7 +8,7 @@
             <a href="/notes" class="text-blue-500 underline">go back...</a>
         </p>
 
-        <p><?= $note['body'] ?></p>
+        <?= htmlspecialchars($note['body']) ?>
     </div>
 </main>
 
